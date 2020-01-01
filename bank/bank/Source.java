97910dbfe3e35ff24039b1ba74a26aba1a128578
First-rays <13080921140@163.com>
2020-01-01 09:45:24 +0800

    2020.01.01

---
+++ bank/bank/Source.java
@@ -0,0 +1,10 @@
+package bank;
+
+import java.util.ArrayList;
+
+public class Source {
+    public static int souNumber = 0;
+
+    public ArrayList<Integer> AVAIL = new ArrayList<>();
+    public ArrayList<Integer> MaxSource = new ArrayList<>();
+}
